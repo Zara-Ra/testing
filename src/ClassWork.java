f9d3f2eb946e1599a829bc41f3285b63520a5393
zahra.rahimi <zahra.rahimi@gmail.com>
2022-09-12 22:47:36 +0430

    another method bla bla bla

--- src/ClassWork.java
+++ src/ClassWork.java
@@ -2,4 +2,7 @@ public class ClassWork {
     public static void main(String[] args) {
         System.out.println("Fourth Line");
     }
+    public static void anotherMethod(){
+        //again bla bla bla
+    }
 }
