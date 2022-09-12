a5ed26d787da3d8fd07995be30136acec46fa7a2
zahra.rahimi <zahra.rahimi@gmail.com>
2022-09-12 22:23:44 +0430

    Second Line commit

--- src/ClassFactory.java
+++ src/ClassFactory.java
@@ -2,5 +2,6 @@ public class ClassFactory {
     public static void main(String[] args) {
         System.out.println("Something in master Branch");
         System.out.println("First line");
+        System.out.println("Second Line");
     }
 }
