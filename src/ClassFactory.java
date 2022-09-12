faed6119230f00e53c06c74a6a6b4d6b1bfee15c
zahra.rahimi <zahra.rahimi@gmail.com>
2022-09-12 22:23:16 +0430

    First Line commit

--- src/ClassFactory.java
+++ src/ClassFactory.java
@@ -1,5 +1,6 @@
 public class ClassFactory {
     public static void main(String[] args) {
         System.out.println("Something in master Branch");
+        System.out.println("First line");
     }
 }
